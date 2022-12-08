3f6b5fa662256e6a99ace492fa1589856c050f20
ammar <ammmar0089@gmail.com>
2022-12-08 21:05:48 +0300

    add code about

--- lib/Screens/AboutUs.dart
+++ lib/Screens/AboutUs.dart
@@ -12,6 +12,16 @@ class aboutUsPage extends StatelessWidget {
           title: Text("About Us"),
         ),
         drawer: DrawerScreen(),
-        body: Center(child: Text("About Us page")));
+        body: Center(child: Text(""" 
+        Here in About us
+
+the latest version is already installed 
+
+TERMS & Condition :
+1- you are above 18 and you respones for all you do in our App
+2-We can provide you with our services here with features, and you can cancel the features, and the conditions may change according to circumstances
+
+Thank you and wish you a pleasant service
+""")));
   }
 }
