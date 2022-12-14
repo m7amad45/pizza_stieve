5b8daf3461ee481e6f513231352c3f17b633a075
omar ALATTAS <omar050464@gmail.com>
2022-12-14 17:01:22 +0300

    about us

--- lib/Screens/AboutUs.dart
+++ lib/Screens/AboutUs.dart
@@ -7,51 +7,53 @@ class aboutUsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: unnecessary_new
     return new Scaffold(
       appBar: AppBar(
-        title: Text("About Us"),
+        title: const Text("About Us"),
       ),
-      body: Center(
-        child: Container(
-          color: Theme.of(context).colorScheme.secondary,
-          child: Text(
-            '''Here in About us
+      
+      body:Center( 
+        child: Text(
+        '''Here in About us
 version 
-the latest version is already installed 
+the latest version is already installed TERMS & Condition :
 
-TERMS & Condition :
+
 1- you are above 18 and you respones for all you do in our App
-2-We can provide you with our services here with features, and you can cancel the features, and the conditions may change according to circumstances
+2-We can provide you with our services here with features, and 
+you can cancel the features, and the conditions may change according 
+to circumstances Thank you and wish you a pleasant service''',
 
-Thank you and wish you a pleasant service''',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-        ),
+
+        style: Theme.of(context).textTheme.headline6,
       ),
-    );
+      ),
+      );
   }
 }
 
-Widget build(BuildContext context) {
-  return new Scaffold(
-      appBar: AppBar(
-        title: Text("About Us"),
-      ),
-      drawer: DrawerScreen(),
-      body: Center(
-          child: Container(
-              color: Theme.of(context).colorScheme.secondary,
-              child: Text(
-                """ 
-        aaa
+// Widget build(BuildContext context) {
+//   // ignore: unnecessary_new
+//   return new Scaffold(
+//       appBar: AppBar(
+//         title: Text("About Us"),
+//       ),
+//       drawer: DrawerScreen(),
+//       body: Center(
+//           child: Container(
+//               color: Theme.of(context).colorScheme.secondary,
+//               child: Text(
+//                 """ 
+//         aaa
 
-""",
-                style: Theme.of(context).textTheme.headline6,
-              ))));
-}
+// """,
+//                 style: Theme.of(context).textTheme.headline6,
+//               ))));
+// }
 
-// i take some of about us from many apps in my phone : 
-//Galary , massage and whatsapp . with some limit action for the user so all control to coder 
+// // i take some of about us from many apps in my phone : 
+// Galary , massage and whatsapp . with some limit action for the user so all control to coder 
 // we make about us page alone not together is to make it easy to mold operations and change becomes
 // flexible with no interfernce in other pages to avoid Errors  
 
